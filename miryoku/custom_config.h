5cbfb84082706a1a82f47f8ac274b4cf9fb97b0b
Isak Larsson <isaklarsson98@hotmail.com>
2023-07-08 19:39:27 +0200

    custom sym layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_SYM \
+&kp SQT,          &kp LESS_THAN,          &kp GREATER_THAN,         &kp DQT,          &kp DOT,          &kp AMPERSAND,              &kp SQT,      &kp RIGHT_BRACKET,     &kp LEFT_BRACKET,       &kp PERCENT,            \
+&kp EXCL,         &kp MINUS,          &kp PLUS,         &kp EQUAL,         &kp HASH,          &kp PIPE,              &kp COLON,         &kp RIGHT_PARENTHESIS,         &kp LEFT_PARENTHESIS,          &kp QUESTION,          \
+&kp CARET,         &kp SLASH,          &kp ASTERISK,            &kp BACKSLASH,          &kp GRAVE,          &kp TILDE,              &kp DOLLAR,       &kp RIGHT_BRACE,     &kp LEFT_BRACE,          &kp AT,              \
+U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
