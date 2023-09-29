8848161c9a82d717bd2a3e30803f596e43f23f8e
Isak Åman Larsson <isak.larsson@savr.com>
2023-09-29 16:37:06 +0200

    formatted layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,10 +5,10 @@
 #define U_TAPPING_TERM 100
 
 #define MIRYOKU_LAYER_SYM \
-&kp SQT,          &kp LESS_THAN,          &kp GREATER_THAN,         &kp DQT,          &kp DOT,          &kp AMPERSAND,              &kp SQT,      &kp RIGHT_BRACKET,     &kp LEFT_BRACKET,       &kp PERCENT,            \
-&kp EXCL,         &kp MINUS,          &kp PLUS,         &kp KP_EQUAL,         &kp HASH,          &kp PIPE,              &kp COLON,         &kp RIGHT_PARENTHESIS,         &kp LEFT_PARENTHESIS,          &kp QUESTION,          \
-&kp CARET,         &kp SLASH,          &kp ASTERISK,            &kp BACKSLASH,          &kp GRAVE,          &kp TILDE,              &kp DOLLAR,       &kp RIGHT_BRACE,     &kp LEFT_BRACE,          &kp AT,              \
-U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+&kp SQT,          &kp LESS_THAN,      &kp GREATER_THAN,    &kp DQT,          &kp DOT,       &kp AMPERSAND,    &kp SQT,      &kp RIGHT_BRACKET,        &kp LEFT_BRACKET,        &kp PERCENT,     \
+&kp EXCL,         &kp MINUS,          &kp PLUS,            &kp KP_EQUAL,     &kp HASH,      &kp PIPE,         &kp COLON,    &kp RIGHT_PARENTHESIS,    &kp LEFT_PARENTHESIS,    &kp QUESTION,    \
+&kp CARET,        &kp SLASH,          &kp ASTERISK,        &kp BACKSLASH,    &kp GRAVE,     &kp TILDE,        &kp DOLLAR,   &kp RIGHT_BRACE,          &kp LEFT_BRACE,          &kp AT,          \
+U_NP,              U_NP,              &kp LPAR,            &kp RPAR,         &kp UNDER,     U_NA,             U_NA,         U_NA,                     U_NP,                    U_NP
 
 #if defined (MIRYOKU_KEYBOARD_CORNE)
 
